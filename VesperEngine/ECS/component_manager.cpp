73126f636f3c9e29f8e0e1308bbed6bcabbac361
Michele Condo <mcondokmb@gmail.com>
2023-09-24 09:20:23 +0100

    Added a copy of ECS from https://github.com/KabalMcBlade/ECS-API

---
+++ VesperEngine/ECS/component_manager.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include "component_manager.h"
+
+ECS_NAMESPACE_BEGIN
+
+std::vector<uint32> ComponentManager::m_componentHashId;
+uint16 ComponentManager::m_maxEntities;
+
+ECS_NAMESPACE_END
---
+++ VesperEngine/ECS/entity_manager.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "entity_manager.h"
+
+ECS_NAMESPACE_BEGIN
+
+std::vector<uint64> EntityManager::m_entities;
+std::vector<uint8> EntityManager::m_entitiesVersion;
+uint16 EntityManager::m_totalEntityCreated;
+
+ECS_NAMESPACE_END
