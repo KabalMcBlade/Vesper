7f4f2e2895d08b6c363e5f678ef53677b0dd8adf
Michele Condo <mcondokmb@gmail.com>
2024-12-17 19:55:00 +0000

    Removed manual copy of ECS - IS NOT BUILDING NOW! - WIP

--- VesperEngine/ECS/component_manager.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-#include "component_manager.h"
-
-ECS_NAMESPACE_BEGIN
-
-std::vector<uint32> ComponentManager::m_componentHashId;
-uint16 ComponentManager::m_maxEntities;
-
-ECS_NAMESPACE_END
