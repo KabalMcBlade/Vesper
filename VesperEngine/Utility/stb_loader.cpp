e6e1d7107506d810be0288c57615194029df3556
Michele Condo <mcondokmb@gmail.com>
2024-12-23 15:39:22 +0000

    Added some stb header only libraries

---
+++ VesperEngine/Utility/stb_loader.cpp
@@ -0,0 +1,15 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "ThirdParty/stb/stb_image.h"
+
+#define __STDC_LIB_EXT1__					// for using sprintf_s
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "ThirdParty/stb/stb_image_resize2.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "ThirdParty/stb/stb_image_write.h"
+
+#define STB_PERLIN_IMPLEMENTATION
+#include "ThirdParty/stb/stb_perlin.h"
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "ThirdParty/stb/stb_truetype.h"
