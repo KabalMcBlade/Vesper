90486303513a57f8da48a417f4dd1336e51570d2
Michele Condo <mcondokmb@gmail.com>
2025-01-08 22:00:41 +0000

    Revert "Finalized custom build batch pipeline for generating and copying assets"
    This reverts commit 6f6efe664043057864e94dbf333833c712a87d77.

--- Viewer/copy_locally.bat
+++ Viewer/copy_locally.bat
@@ -8,37 +8,49 @@ echo:
 if "%~1"=="" goto :END_1
 if "%~2"=="" goto :END_2
 if "%~3"=="" goto :END_3
-if "%~4"=="" goto :END_4
 
-if not exist "%~3Assets\%~1" (
-    echo Folder %~3Assets\%~1 not found, creating it.
-    mkdir "%~3Assets\%~1"
+if not exist "%~2Assets\Shaders" (
+    echo Folder %~2Assets\Shaders not found, creating it.
+    mkdir "%~2Assets\Shaders"
 )
 
+if not exist "%~2Assets\Models" (
+    echo Folder %~2Assets\Models not found, creating it.
+    mkdir "%~2Assets\Models"
+)
+
+if not exist "%~2Assets\Textures" (
+    echo Folder %~2Assets\Textures not found, creating it.
+    mkdir "%~2Assets\Textures"
+)
 echo:
 
-echo Copying assets files from %~2Assets\%~1\ to %~3Assets\%~1\:
-copy "%~2Assets\%~1\*.*" "%~3Assets\%~1\"
+echo Copying shader files from %~1Assets\Shaders\ to %~2Assets\Shaders\:
+copy "%~1Assets\Shaders\*.spv" "%~2Assets\Shaders\"
+echo:
+
+echo Copying model files from %~1Assets\Models\ to %~2Assets\Models\:
+copy "%~1Assets\Models\*.*" "%~2Assets\Models\"
+echo:
+
+echo Copying texture files from %~1Assets\Textures\ to %~2Assets\Textures\:
+copy "%~1Assets\Textures\*.*" "%~2Assets\Textures\"
 echo:
 
 :: Create the timestamp file here if everything is successful
-echo. > "%~4copy_%~1_build.timestamp"
+echo. > "%~3copy_assets_build.timestamp"
 
 goto :END_OK
 
 :END_1
-echo No asset folder provided as parameter!
-exit /b 1
-
-:END_2
 echo No $(ProjectDir) provided as parameter!
 exit /b 1
 
-:END_3
+:END_2
 echo No $(OutDir) provided as parameter!
 exit /b 1
 
-:END_4
+:END_3
 echo No $(IntDirFullPath) provided as parameter!
 exit /b 1
 
